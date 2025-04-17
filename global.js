014a44be8e127f76f1537f1e4fc98247d39b150c
d3kong <d3kong@ucsd.edu>
2025-04-16 20:56:23 -0700

    Highlight current page in nav

--- global.js
+++ global.js
@@ -4,3 +4,10 @@ function $$(selector, context=document) {
     return Array.from(context.querySelectorAll(selector));
 }
 
+const currentPath = location.pathname.split("/").pop() || "index.html";
+
+$$("nav a").forEach((a) => {
+    if (a.getAttrirbute("href").endsWith(currentPath)) {
+        a.setAttribute("aria-current", "page");
+    }
+})
