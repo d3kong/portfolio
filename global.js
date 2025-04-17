dc83a9c45892eba79f96446e6cdf5dc1a5a6237c
d3kong <d3kong@ucsd.edu>
2025-04-16 21:53:20 -0700

    Add dark mode toggle

--- global.js
+++ global.js
@@ -11,3 +11,17 @@ $$("nav a").forEach((a) => {
         a.setAttribute("aria-current", "page");
     }
 })
+
+const themeButton = document.getElementById("toggle-theme");
+
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme) {
+    document.documentElement.setAttribute("data-theme", savedTheme);
+}
+
+themeButton?.addEventListener("click", () => {
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+    document.documentElement.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+})
