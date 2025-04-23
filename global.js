f6862282e5423bb4e8200ddb10a50d8a3ea81c21
d3kong <d3kong@ucsd.edu>
2025-04-23 16:52:24 -0700

    Use reusable fetchJSON function

--- global.js
+++ global.js
@@ -1,5 +1,20 @@
 console.log("IT'S ALIVE");
 
+export async function fetchJSON(url) {
+    try {
+      const response = await fetch(url);
+  
+      if (!response.ok) {
+        throw new Error(`Failed to fetch projects: ${response.statusText}`);
+      }
+  
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching or parsing JSON data:', error);
+    }
+  }
+
 function $$(selector, context=document) {
     return Array.from(context.querySelectorAll(selector));
 }
