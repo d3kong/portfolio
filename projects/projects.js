dcee4896fdda2e599db1b76b907949d8987e4fcd
d3kong <d3kong@ucsd.edu>
2025-04-23 18:57:48 -0700

    Safely handle undefined project list in title

--- projects/projects.js
+++ projects/projects.js
@@ -16,17 +16,23 @@ function renderProjects(projects, container, headingLevel = 'h2') {
 async function loadAndRenderProjects() {
     try {
         const projects = await fetchJSON("/portfolio/lib/projects.json");
+  
         const container = document.querySelector(".projects");
         if (projects && container) {
+            console.log("Fetched projects:", projects);
             renderProjects(projects, container);
+    
             const title = document.querySelector(".page-title");
-            if (title) {
+    
+            if (title && Array.isArray(projects)) {
                 title.textContent = `${projects.length} Projects`;
+            } else if (title) {
+                title.textContent = `Projects`;
             }
         }
     } catch (error) {
         console.error("Error loading projects: ", error);
     }
-}
+}  
 
 loadAndRenderProjects();
