035bff1f9ea09c18bc8a6d74cc20dfc29ab0663c
d3kong <d3kong@ucsd.edu>
2025-04-23 18:26:41 -0700

    Created a dynamic way to load and render uploaded projects

---
+++ projects/projects.js
@@ -0,0 +1,32 @@
+import { fetchJSON, $$ } from "/portfolio/global.js";
+
+function renderProjects(projects, container, headingLevel = 'h2') {
+    container.innerHTML = "";
+    projects.forEach(project => {
+        const article = document.createElement("article");
+        article.innerHTML = `
+            <${headingLevel}>${project.title}</${headingLevel}>
+            <img src="${project.image}" alt="Screenshot of ${project.title}">
+            <p>${project.description}</p>
+        `;
+        container.appendChile(article);
+    });
+}
+
+async function loadAndRenderProjects() {
+    try {
+        const projects = await fetchJSON("/portfolio/lib/projects.json");
+        const container = document.querySelector(".projects");
+        if (projects && container) {
+            renderProjects(projects, container);
+            const title = document.querySelector(".page-title");
+            if (title) {
+                title.textContent = `${projects.lenght} Projects`
+            }
+        }
+    } catch (error) {
+        console.error("Error loading projects: ", error);
+    }
+}
+
+loadAndRenderProjects();
